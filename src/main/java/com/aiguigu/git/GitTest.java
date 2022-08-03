1bb758942ed4fc0551849626174876f4de0f09ec
hzx <hzx@qq.com>
2022-08-03 15:41:31 +0800

    fitst commit

---
+++ src/main/java/com/aiguigu/git/GitTest.java
@@ -0,0 +1,11 @@
+package com.aiguigu.git;
+
+/**
+ * @author zhenxing
+ * @creater 2022-08-03 15:36
+ */
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("hello git!");
+    }
+}
